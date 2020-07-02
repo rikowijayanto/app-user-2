6831274e49bc9dd70517f660c95855841d5cb91b
Riko <mr.rikowijayanto@gmail.com>
2020-07-03 02:23:55 +0700

    Add splash on welcome screen

---
+++ Homepage/app/src/main/java/com/example/homepage/home.java
@@ -0,0 +1,35 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary)); // Navigation bar the soft bottom of some phones like nexus and some Samsung note series
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimary)); //status bar or the time bar at the top
+        }
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(home.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+
+
+    }
+}
