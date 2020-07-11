ffa76676c96111d1b02a49b3d630ecf7051d8d89
Riko <mr.rikowijayanto@gmail.com>
2020-07-11 22:29:52 +0700

    Localization

--- Homepage/app/src/main/java/com/example/homepage/ui/home/HomeFragment.java
+++ Homepage/app/src/main/java/com/example/homepage/ui/home/HomeFragment.java
@@ -3,7 +3,9 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
@@ -137,5 +139,17 @@ public class HomeFragment extends Fragment {
 
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.keluar) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        } else if (item.getItemId() == R.id.language) {
+            Intent mIntent = new Intent (Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+
+        return true;
     }
 }
