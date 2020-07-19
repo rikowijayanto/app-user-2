fdeb3f6748c6038fc066c712b8fa641715e57383
Riko <mr.rikowijayanto@gmail.com>
2020-07-19 15:50:30 +0700

    Refactor 7

--- Homepage/app/src/main/java/com/example/homepage/FavouriteFragment.java
+++ Homepage/app/src/main/java/com/example/homepage/FavouriteFragment.java
@@ -11,17 +11,16 @@ import android.view.ViewGroup;
 
 public class FavouriteFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
     public FavouriteFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -47,7 +46,7 @@ public class FavouriteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+       
         return inflater.inflate(R.layout.fragment_favourite, container, false);
     }
 }
